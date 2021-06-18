d05b27c917a762426cc6a6dddac944265b73395f
artn-dev <azemarteixeira@gmail.com>
2021-06-17 23:45:48 -0300

    Add twitter button

---
+++ src/components/TwitterButton.js
@@ -0,0 +1,16 @@
+const TwitterButton = () => {
+  return (
+    <>
+      <a
+        id="tweet-quote"
+        className="btn btn-outline-info ms-2"
+        href="https://twitter.com/intent/tweet"
+        target="_blank"
+      >
+        <i className="bi bi-twitter"></i>
+      </a>
+    </>
+  )
+}
+
+export default TwitterButton
